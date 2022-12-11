2b1c623b0d9a91953cbf8d5070ab3e7236217e80
Andrea0o0 <andreagarciagonzalez22@gmail.com>
2022-12-11 17:24:36 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,65 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    let list
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    if(this.length==0||this.length<=2||this.length>1){
+      this.items.push(item)
+      this.items = this.items.sort((a, b) => a - b)
+      this.length++
+    }
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.items[pos]!=undefined){
+      return this.items[pos]
+    }
+    else{
+      return error      
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.items.length>0){
+      return Math.max(...this.items) 
+    }
+    else{
+      return error
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.items.length>0){
+      return Math.min(...this.items) 
+    }
+    else{
+      return error
+    }
+  }
 
-  sum() {}
+  sum() {
+    if(this.items.length>0){
+      return this.items.reduce(
+        (accum, currentV) => accum + currentV) 
+    }
+    else{
+      return 0
+    }
+  }
 
-  avg() {}
+  avg() {
+    if(this.items.length>0){
+      return this.sum()/this.items.length
+    }
+    else{
+      return error
+    }
+  }
 }
 
 module.exports = SortedList;
+
+
